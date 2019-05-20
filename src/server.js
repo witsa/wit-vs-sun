dfd28d49c349be8a84b2220981f64b6a0edadbcf
Emmanuel FITOUSSI <emmanuel.fitoussi@wit.fr>
2019-05-20 14:39:59 +0200

    test lowdb avec lodash-id

--- src/server.js
+++ src/server.js
@@ -1,6 +1,27 @@
-import * as express from 'express';
-import * as http from 'http';
-import * as WebSocket from 'ws';
+"use strict";
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+
+const low = require("lowdb");
+const lodashId = require('lodash-id')
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync('db/store.json');
+const db = low(adapter);
+db._.mixin(lodashId);
+
+// Set some defaults
+db.defaults({ messages: [] })
+  .write();
+
+// // Add a post
+// db.get('posts')
+//   .insert({ title: 'lowdb is awesome' })
+//   .write();
+
+// // Set a user using Lodash shorthand syntax
+// db.set('user.name', 'typicode')
+//   .write();
 
 const app = express();
 
@@ -10,16 +31,20 @@ const server = http.createServer(app);
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws: WebSocket) => {
+wss.on('connection', (ws) => {
 
   //connection is up, let's add a simple simple event
-  ws.on('message', (message: string) => {
+  ws.on('message', (message) => {
 
     //log the received message and send it back to the client
     console.log('received: %s', message);
-    // parse message
 
+    // parse message
     var messObj = JSON.parse(message);
+    messObj.dt = new Date();
+    db.get('messages')
+      .insert(messObj)
+      .write();
 
     if (messObj.isBroadcast) {
 
@@ -30,8 +55,8 @@ wss.on('connection', (ws: WebSocket) => {
             client.send(JSON.stringify({ type: 'message', data: '[client] ' + messObj.data }));
           }
         });
-
-    } else {
+    }
+    else {
       ws.send(JSON.stringify({ type: 'message', data: '[server] ' + messObj.data }));
     }
   });
